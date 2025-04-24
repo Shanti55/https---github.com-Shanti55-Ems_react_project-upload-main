8977f6a0333840863232c8ad4943279e6b902acb
Shanti55 <shantik953@gmail.com>
2025-04-24 10:19:17 +0530

    first commit

---
+++ src/utils/localStorage.jsx
@@ -0,0 +1,35 @@
+// utils/localStorage.js
+
+const employees = [
+    // ... tera pura employee data yahan
+  ];
+  
+  const admin = [
+    {
+      id: 1,
+      email: "admin@me.com", // ya jo bhi email tu App.jsx mein use kar rahi hai
+      password: "123"
+    }
+  ];
+  
+  export const setLocalStorage = () => {
+    const existingEmployees = localStorage.getItem('employees');
+    const existingAdmin = localStorage.getItem('admin');
+  
+    if (!existingEmployees) {
+      localStorage.setItem('employees', JSON.stringify(employees));
+    }
+  
+    if (!existingAdmin) {
+      localStorage.setItem('admin', JSON.stringify(admin));
+    }
+  };
+  
+  export const getLocalStorage = () => {
+    const employees = JSON.parse(localStorage.getItem('employees'));
+    const admin = JSON.parse(localStorage.getItem('admin'));
+  
+    return { employees, admin };
+  };
+  
+                
